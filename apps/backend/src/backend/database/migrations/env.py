e18cdaa50265b037adf4b8eef1baed6286a01727
NanoClem <clemdecoop.pro@gmail.com>
2026-09-20 23:53:30 +0200

    refactor : move alembic migrations into database module

--- apps/backend/src/backend/database/migrations/env.py
+++ apps/backend/src/backend/database/migrations/env.py
@@ -8,9 +8,8 @@ from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
-from backend.core.config import db_config  # isort: skip
-from backend.users.models import UserModel  # isort: skip
-from backend.common.models import Base  # isort: skip
+from backend.database.config import db_config
+from backend.database.migrations.model_registry import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
---
+++ apps/backend/src/backend/database/migrations/model_registry.py
@@ -0,0 +1,2 @@
+from backend.users import models  # noqa: F401, isort: skip
+from backend.common.models import Base  # noqa: F401, isort: skip
